9dd761597051fae7ded592c00b5c4189b5570229
정태윤 <112293078+wjdxodbs@users.noreply.github.com>
2025-06-07 22:40:45 +0900

    [Silver V] Title: Olympiad Pizza, Time: 156 ms, Memory: 11648 KB -BaekjoonHub

--- 백준/Silver/15235. Olympiad Pizza/Olympiad Pizza.js
+++ 백준/Silver/15235. Olympiad Pizza/Olympiad Pizza.js
@@ -3,21 +3,22 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 let n = +input[0];
 let arr = input[1].split(' ').map(Number);
+let queue = arr.map((v, i) => ({ id: i, cnt: v }));
+let result = Array(n).fill(0);
+let front = 0;
+let time = 0;
 
-let queue = [];
-let result = Array.from({ length: n }, () => 0);
+while (front < queue.length) {
+  const item = queue[front];
+  front++;
+  time++;
+  item.cnt--;
 
-while (!arr.every((item) => item === 0)) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i]) {
-      queue.push(i);
-      arr[i] -= 1;
-    }
+  if (item.cnt === 0) {
+    result[item.id] = time;
+  } else {
+    queue.push(item);
   }
 }
-
-arr.forEach((_, idx) => {
-  result[idx] = queue.lastIndexOf(idx) + 1;
-});
 
 console.log(result.join(' '));
