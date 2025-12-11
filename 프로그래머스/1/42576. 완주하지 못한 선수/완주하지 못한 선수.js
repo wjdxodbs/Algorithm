9a13813e78011ace77f5215082a7df28dca80f63
정태윤 <112293078+wjdxodbs@users.noreply.github.com>
2025-12-11 22:49:33 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 33.86 ms, Memory: 67.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,23 +1,18 @@
 function solution(participant, completion) {
     let map = new Map();
     
-    for (let elm of participant){
-        if(map.has(elm)){
-            map.set(elm, map.get(elm)+1)
-        } else {
-            map.set(elm, 1)
-        }
+    for (let i = 0; i < participant.length; i++){
+        let a = participant[i];
+        let b = completion[i]
+        
+        map.set(a, (map.get(a)||0)+1)
+        map.set(b, (map.get(b)||0)-1)
     }
     
-    for (let elm of completion){
-        if(map.has(elm)){
-            map.set(elm, map.get(elm)-1)
-        } 
-    }
-    
-    for (let elm of map){
-        if (elm[1] !== 0){
-            return elm[0]
+
+    for (let [k, v] of map){
+        if (v > 0){
+            return k
         }
     }
 }
