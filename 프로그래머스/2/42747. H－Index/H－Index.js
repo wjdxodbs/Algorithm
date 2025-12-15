fd1d65cdf91f1d1f677f6301f8a56271b624551e
정태윤 <112293078+wjdxodbs@users.noreply.github.com>
2025-12-15 23:19:28 +0900

    [level 2] Title: H-Index, Time: 0.61 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -0,0 +1,14 @@
+function solution(citations) {
+    let arr = [...citations].sort((a,b)=>a-b);
+    let l = citations.length;
+    
+    for(let i = 0; i < l; i++){
+        let num = l - i;
+        
+        if (arr[i] >= num) {
+            return num
+        }
+    }
+    
+    return 0
+}
