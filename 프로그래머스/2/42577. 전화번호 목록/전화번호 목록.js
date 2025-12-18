25023e7620fabc10f2a6c9481b48c93dfeb8d3fc
정태윤 <112293078+wjdxodbs@users.noreply.github.com>
2025-12-18 22:51:51 +0900

    [level 2] Title: 전화번호 목록, Time: 155.16 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,19 +1,9 @@
 function solution(phone_book) {
-    // 해시맵 생성
-    const hash = new Map();
+    let arr = [...phone_book].sort();
     
-    // 모든 전화번호를 해시맵에 저장
-    for (const phone of phone_book) {
-        hash.set(phone, true);
-    }
-    
-    // 각 전화번호에 대해 접두어 검사
-    for (const phone of phone_book) {
-        for (let i = 1; i < phone.length; i++) {
-            const prefix = phone.slice(0, i);
-            if (hash.has(prefix)) {
-                return false;
-            }
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i + 1].startsWith(arr[i])) {
+            return false;
         }
     }
     
