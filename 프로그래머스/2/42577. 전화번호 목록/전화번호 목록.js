efcfa8feb4ad9ead0dc9884d285160082f8e9619
정태윤 <112293078+wjdxodbs@users.noreply.github.com>
2026-01-17 00:44:57 +0900

    [level 2] Title: 전화번호 목록, Time: 164.97 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,11 +1,15 @@
 function solution(phone_book) {
-    let arr = [...phone_book].sort();
+    phone_book.sort();
     
-    for (let i = 0; i < arr.length - 1; i++) {
-        if (arr[i + 1].startsWith(arr[i])) {
-            return false;
+    for (let i = 1; i < phone_book.length; i++){
+        let len1 = phone_book[i-1].length;
+        let len2 = phone_book[i].length;
+        let minLen = Math.min(len1, len2);
+        
+        if (phone_book[i].slice(0, minLen) === phone_book[i-1].slice(0, minLen)){
+            return false
         }
     }
     
-    return true;
+    return true
 }
