b1691203dc6e212350f6bfb066f7d6c592e26d8e
정태윤 <112293078+wjdxodbs@users.noreply.github.com>
2026-01-19 04:45:05 +0900

    [level 2] Title: 기능개발, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,17 +1,26 @@
 function solution(progresses, speeds) {
-    let arr = progresses.map((elm, idx) => Math.ceil((100-elm)/speeds[idx]))
-    let result = [1]
-    let spot = arr[0]
+    let newProgresses = progresses.map((e, i)=>{
+        let a = 100-e;
+        let day = Math.ceil(a / speeds[i])
+        return day
+    })
     
+    let elm = 1;
+    let maxNum = newProgresses[0];
+    let result = [];
     
-    for (let i = 1; i < arr.length; i++){
-        if (arr[i] > spot){
-            result.push(1)
-            spot = arr[i]
-        } else {
-            result[result.length-1] += 1
+    for (let i = 1; i < newProgresses.length; i++){
+        if (maxNum < newProgresses[i]){
+            result.push(elm)
+            maxNum = newProgresses[i]
+            elm = 1;
+            continue;
         }
+        
+        elm += 1
     }
+    
+    result.push(elm)
     
     return result;
 }
